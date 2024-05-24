0388835d59e0d13492205c43efcd834444bdcbbb
thami88 <thaminduliyanage97@gmail.com>
2024-05-24 14:36:35 +0530

    [added] Job model and refactor router

---
+++ app/Models/Job.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Support\Arr;
+
+class Job {
+    public static function all(): array
+    {
+        return [
+            [
+                'id' => 1,
+                'title' => 'Director',
+                'salary' => '$50000'
+            ],
+            [
+                'id' => 2,
+                'title' => 'Developer',
+                'salary' => '$10000'
+            ],
+            [
+                'id' => 3,
+                'title' => 'Teacher',
+                'salary' => '$40000'
+            ]
+        ];
+    }
+
+    public static function find(int $id): array
+    {
+        $job = Arr::first(static::all(), fn($job) => $job['id']==$id);
+
+        if (! $job) {
+            abort(404);
+        }
+        
+        return $job;
+    }
+}
