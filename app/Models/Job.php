9b24c1beed1a21234d2a7161f96aa3a5cd3b275c
thami88 <thaminduliyanage97@gmail.com>
2024-05-25 13:54:10 +0530

    [added] Post class migrations and fixed mass assignment issue

--- app/Models/Job.php
+++ app/Models/Job.php
@@ -2,38 +2,9 @@
 
 namespace App\Models;
 
-use Illuminate\Support\Arr;
+use Illuminate\Database\Eloquent\Model;
 
-class Job {
-    public static function all(): array
-    {
-        return [
-            [
-                'id' => 1,
-                'title' => 'Director',
-                'salary' => '$50000'
-            ],
-            [
-                'id' => 2,
-                'title' => 'Developer',
-                'salary' => '$10000'
-            ],
-            [
-                'id' => 3,
-                'title' => 'Teacher',
-                'salary' => '$40000'
-            ]
-        ];
-    }
-
-    public static function find(int $id): array
-    {
-        $job = Arr::first(static::all(), fn($job) => $job['id']==$id);
-
-        if (! $job) {
-            abort(404);
-        }
-        
-        return $job;
-    }
+class Job extends Model{
+    protected $table = 'job_listings';
+    protected $fillable = ['title', 'salary'];
 }
